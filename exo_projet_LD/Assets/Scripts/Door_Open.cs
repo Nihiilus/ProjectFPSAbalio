cc3c79a0d55ce23bc9206d69a27f5552172dba5b
9638_gdes (Student) <s_9638_gdes@PEDAGOGIQUE.LAN>
2019-11-19 15:35:26 +0100

    LastFinalUltimateVersion1.0

---
+++ exo_projet_LD/Assets/Scripts/Door_Open.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Open : MonoBehaviour
+{
+    [SerializeField]
+
+    GameObject Door_Dank_A_snaps010;
+
+    bool isOpened = false;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            Door_Dank_A_snaps010.transform.Rotate ( new Vector3 (0,-76,0) );
+        }
+        
+    }
+}
