33b21b1330bc6cc1b40ec39eb2c2ce1d983bc4ae
9638_gdes (Student) <s_9638_gdes@PEDAGOGIQUE.LAN>
2019-11-20 14:27:50 +0100

    Module message

---
+++ exo_projet_LD/Assets/Scripts/ReadingNotes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadingNotes : MonoBehaviour
+{
+    public GameObject IconReading;
+    public GameObject PageReading;
+    bool toggleBool = false;
+    public AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Notes"))
+            IconReading.SetActive(true);
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyUp("e"))
+        {
+            toggleBool = !toggleBool;
+            PageReading.SetActive(toggleBool);
+            audioSource.Play();
+        }
+            
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Notes"))
+            IconReading.SetActive(false);
+        PageReading.SetActive(false);
+
+    }
+}
+
+
+
