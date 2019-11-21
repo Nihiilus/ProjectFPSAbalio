3ad917c0ba588812db8e279206247235a20861aa
9638_gdes (Student) <s_9638_gdes@PEDAGOGIQUE.LAN>
2019-11-21 13:39:40 +0100

    Commit PostBuild 13h30

---
+++ exo_projet_LD/Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour
+{
+    public GameObject objectToDestroy; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            Destroy(objectToDestroy);
+        }
+    }
+}
